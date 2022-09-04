e7ab309402b122c84b65308486b0e5857cfceef4
Alex House <ahouse101@gmail.com>
2022-09-04 16:21:10 -0700

    Created minimal PHP file to get started on comments report.

---
+++ report.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang='en'>
+	<head>
+		<title>Comments Report</title>
+		<meta charset='utf-8'>
+	</head>
+
+	<body>
+		<h1>Testing!</h1>
+
+		<?php phpinfo() ?>
+	</body>
+</html>
